bc4f45e5ccd1a240beeb42bb64fe32e03248da84
FjellOverflow <fjelloverflow@protonmail.com>
2025-05-14 23:41:44 +0300

    feat: background image

--- generator/templates/BasicTemplate.tsx
+++ generator/templates/BasicTemplate.tsx
@@ -1,35 +1,44 @@
-import type { ReactNode } from "react";
-import Footer, { type FooterProps } from "../components/Footer";
-import Header, { type HeaderProps } from "../components/Header";
+import type { ReactNode } from 'react'
+import Footer, { type FooterProps } from '../components/Footer'
+import Header, { type HeaderProps } from '../components/Header'
 
-export interface BasicTemplateProps {
-	header?: HeaderProps;
-	footer?: FooterProps;
-	backgroundColorHex?: `#${string}`;
-	children?: ReactNode;
+export type BasicTemplateProps = {
+	header?: HeaderProps
+	footer?: FooterProps
+	children?: ReactNode
+	backgroundColorHex?: `#${string}`
+	backgroundImageUrl?: string
 }
 
 export default function BasicTemplate({
 	children,
 	header,
 	footer,
-	backgroundColorHex = "#E10600",
+	backgroundColorHex = '#E10600',
+	backgroundImageUrl
 }: BasicTemplateProps): ReactNode {
+	const style = {} as Record<string, string>
+
+	if (backgroundImageUrl) {
+		style.backgroundImage = `url(${backgroundImageUrl})`
+		style.backgroundSize = 'cover'
+	} else style.backgroundColor = backgroundColorHex
+
 	return (
 		<div
 			style={{
-				backgroundColor: backgroundColorHex,
-				display: "flex",
-				height: "100%",
-				width: "100%",
-				flexDirection: "column",
+				display: 'flex',
+				height: '100%',
+				width: '100%',
+				flexDirection: 'column',
+				...style
 			}}
 		>
 			<div
 				style={{
-					display: "flex",
-					padding: "32px",
-					color: header?.textColorHex || "#FFFFFF",
+					display: 'flex',
+					padding: '32px',
+					color: header?.textColorHex || '#FFFFFF'
 				}}
 			>
 				<Header {...header} />
@@ -37,7 +46,7 @@ export default function BasicTemplate({
 
 			<div
 				style={{
-					display: "flex",
+					display: 'flex'
 				}}
 			>
 				{children}
@@ -45,13 +54,13 @@ export default function BasicTemplate({
 
 			<div
 				style={{
-					marginTop: "auto",
-					display: "flex",
-					padding: "32px",
+					marginTop: 'auto',
+					display: 'flex',
+					padding: '32px'
 				}}
 			>
 				<Footer {...footer} />
 			</div>
 		</div>
-	);
+	)
 }
